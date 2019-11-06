0eb92ebde96fa5d98372fce851a84a08dc918bd6
MoonChaserChen <moon_chaser@163.com>
2019-11-06 17:25:57 +0800

    脚本细节修改

--- gen_sidebar.py
+++ gen_sidebar.py
@@ -2,18 +2,18 @@ import os
 
 suffix = ".md"
 gen_file = "_sidebar.md"
-ignore_folders = [".git"]
+ignore_files = [gen_file, ".git", "README.md"]
 
 
 def print_file(c_dir, depth, write2):
     for f in os.listdir(c_dir):
         re_f = os.path.join(c_dir, f)
         is_d = os.path.isdir(re_f)
-        if(is_d and f not in ignore_folders):
+        if is_d and f not in ignore_files:
             write2.write("\t" * depth + "- " + f + "\n")
             print_file(re_f, depth + 1, write2)
         else:
-            if (suffix in f and gen_file not in f):
+            if suffix in f and f not in ignore_files:
                 f_n = os.path.splitext(f)[0]
                 write2.write("\t" * depth + "- [" + f_n + "](/" + re_f + ")\n")
 
